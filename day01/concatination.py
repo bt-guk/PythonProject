acce3bd6dd4a5fbe1f90a8b64516cd8dbb0fcf8d
bt-guk <beck7960@gmail.com>
2025-04-13 23:45:12 -0400

    concatination

---
+++ day01/concatination.py
@@ -0,0 +1,5 @@
+prog_lang = "python"
+version = 3.13
+
+print("My programming language is {} and the version is {}.".format(prog_lang, version))
+print(f'My programming language is {prog_lang} and the version is {version}')
