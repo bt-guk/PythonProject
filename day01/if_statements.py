1b8f954aa5f2607bf60c8d9d83f350bb6b80bd75
bt-guk <beck7960@gmail.com>
2025-04-14 00:15:02 -0400

    if statements

---
+++ day01/if_statements.py
@@ -0,0 +1,44 @@
+browser_name = "firefox"
+
+if browser_name == "chrome" :
+    print("Chrome Browser is Selected")
+
+
+if browser_name == "chrome" :
+    print("Chrome Browser is Selected")
+else:
+    print("Firefox Browser is Select")
+
+if browser_name == "chrome" :
+    print("Chrome Browser is Selected")
+elif browser_name == "firefox":
+    print("Firefox Browser is Select")
+else:
+    print("Safari Browser is Select")
+
+print('==================================')
+
+score = 80
+
+#use pass keyword as a place holder for code
+
+if  0 <= score <= 100:
+    if score >= 60 :
+        pass
+    else:
+        pass
+else:
+    pass
+
+
+if  0 <= score <= 100:
+    if score >= 60 :
+        print('Passed the exam')
+    else:
+        print('Failed exam')
+else:
+    print('Invalid score')
+
+
+
+
