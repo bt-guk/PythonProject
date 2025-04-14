32cc4b2b094a1c5e5d61aeb7438f66cf47baede3
bt-guk <beck7960@gmail.com>
2025-04-14 17:07:33 -0400

    string

---
+++ day02/string_review.py
@@ -0,0 +1,21 @@
+n = 'Automation Testing'
+
+print(n[0]) # first index
+print(n[-1]) # last index
+print(n[len(n) -1]) # last index
+print(len(n)) # length
+
+
+#slicing
+
+print("==============================")
+
+s = 'Python Programming Language'
+s1 = s[7:] #including 7th index till the end
+print(s1)
+s2 = s[:18] # beginning to end
+print(s2)
+
+reversed = s[::-1] # :: to make it slicing from the end
+print(reversed)
+
